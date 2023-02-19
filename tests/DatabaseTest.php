503a0657139e6d5777c91558a12ebf0868c4492a
hassanec3u <hassanec3u@etu.univ-lorraine.fr>
2023-02-19 18:54:40 +0100

    exemple de test de la bdd, a vous de tester et de me signaler des problemes ! ;)

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Tests\Entity;
+namespace App\Tests;
 
+use App\Entity\Trajet;
 use App\Entity\Utilisateur;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -17,7 +18,7 @@ class DatabaseTest extends KernelTestCase
 
     public function testUtilisateur()
     {
-        //--------------------------------zone de test --------------
+        //--------------------------------zone de test et exemple--------------
         $user = new Utilisateur();
         $user->setAdresseMail("abou@toto.fr");
         $user->setMdp("1234");
@@ -28,15 +29,25 @@ class DatabaseTest extends KernelTestCase
         $user->setNoTel("12345678");
         $user->setMailNotif("toto@gk.com");
 
-
+        $trajet = new Trajet();
+        $trajet->setLieuDepart("Nancy");
+        $trajet->setLieuArrive("Paris");
+        $trajet->setDateHeureDepart(new \DateTime('now'));
+        $trajet->setPrix(40);
+        $trajet->setCapaciteMax(5);
+        $trajet->setPrecisionLieuRdv("ok");
+        $trajet->setCommentaire("ok");
+        $trajet->setCovoitureur($user);
 
         $this->entityManager->persist($user);
+        $this->entityManager->persist($trajet);
         $this->entityManager->flush();
 
-        print ($user->getId());
 
-        $this->assertEquals("abou@toto.fr",$user->getAdresseMail());
+        $this->assertEquals("abou@toto.fr", $user->getAdresseMail());
+        $this->assertEquals($user->getId(),$trajet->getCovoitureur()->getId());
     }
+
     protected function tearDown(): void
     {
         parent::tearDown();
