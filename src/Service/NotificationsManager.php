32fce6a17b7b84137fbb3ae9b3800eeb261d1072
joffroy8u <theo.joffroy7@etu.univ-lorraine.fr>
2023-03-07 21:53:33 +0100

    Ajout du constructeur du manager

--- src/Service/NotificationsManager.php
+++ src/Service/NotificationsManager.php
@@ -2,8 +2,28 @@
 
 namespace App\Service;
 
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\NotifReponseRepository;
+use App\Repository\NotifAnnulationRepository;
+use App\Repository\NotifTrajetPriveRepository;
+
 class NotificationsManager
 {
+    private $notifsAnnulation;
+    private $notifsReponse;
+    private $notifsTrajetPrive;
+    private $entityManager;
+
+    public function __construct(NotifAnnulationRepository $notifsAnnulation,
+                                NotifReponseRepository $notifsReponse,
+                                NotifTrajetPriveRepository $notifsTrajetPrive,
+                                EntityManagerInterface $entityManager)
+    {
+        $this->notifsAnnulation = $notifsAnnulation;
+        $this->notifsReponse = $notifsReponse;
+        $this->notifsTrajetPrive = $notifsTrajetPrive;
+        $this->entityManager = $entityManager;
+    }
     
 }
 
