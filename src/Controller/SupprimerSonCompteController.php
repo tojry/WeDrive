7cb2bc6f82a5712007e74e64c44d0848128f305c
nyela20 <ralantonisainananyela@gmail.com>
2023-03-14 09:47:14 +0100

    Premier commit (base html/css) du formulaire et base php Symfony

--- src/Controller/SupprimerSonCompteController.php
+++ src/Controller/SupprimerSonCompteController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Utilisateur;
 
 class SupprimerSonCompteController extends AbstractController
 {
@@ -15,4 +18,26 @@ class SupprimerSonCompteController extends AbstractController
             'controller_name' => 'SupprimerSonCompteController',
         ]);
     }
+
+    #[Route('/supprimerCompte/{id}', name: 'utilisateur.supprimerCompte')]
+    public function supprimerUnCompteSuperUser(Utilisateur $utilisateur = null, ManagerRegistry $doctrine ) : RedirectResponse {
+        
+        // Récuperer la personne
+        /*
+        if($utilisateur){
+            // Si la personne existe => le supprimer et retourner un flashMessage de succès
+            $manager = $doctrine->getManager();
+            // Ajoute la fonction de suppression dans la transaction
+            $manager->remove($utilisateur);
+            //Exécuter la transation
+            $manager->flush();
+            $this->addFlash('success','La personne a été supprimer avec succès');
+        } else {
+            // Sinon retourner un flashMessage d'erreur
+            $this->addFlash('error','Personne inexistante');
+        }
+        return $this->redirectToRoute("app_supprimer_un_compte_super_user");
+        */
+    }
+
 }
