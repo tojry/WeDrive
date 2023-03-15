77b0499e97ce4e867567e4fe7fb4aa0b0f2e2f6c
slimani44u <slimani44u@etu.univ-lorraine.fr>
2023-03-15 21:07:41 +0100

    il manque l'interface

--- src/Controller/ConsulercompteController.php
+++ src/Controller/ConsulercompteController.php
@@ -5,17 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UtilisateurRepository;
 
 class ConsulercompteController extends AbstractController
 {
     #[Route('/consulercompte', name: 'app_consulercompte')]
-    public function consultercompte(): Response
+    public function consultercompte(UtilisateurRepository  $utilisateurRepository): Response
     {
+        $listUsers = $utilisateurRepository->findAll();
         $user = $this->getUser();
         if ($user) {
             $id = $user->getId();
             $adresseMail = $user->getAdresseMail();
-            $mdp = $user->getMdp();
             $nom = $user->getNom();
             $prenom = $user->getPrenom();
             $sexe = $user->getSexe();
@@ -37,12 +38,14 @@ class ConsulercompteController extends AbstractController
 
         } else {
             // L'utilisateur n'est pas connecté
-            echo "Vous n'êtes pas connecté";
+
+            return new Response("Vous n'êtes pas connecté",502);
         }
         return $this->render('consulercompte/index.html.twig',  [
-            'user' => $user,
+            'user_co' => $user,
              'voiture' => $voiture,
              'notif' => $notif,
+            'listusers'=> $listUsers,
         ]);
     }
 }
