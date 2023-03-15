dd78d707ac281149ccba263ee00d942fd4a05e33
nyela20 <ralantonisainananyela@gmail.com>
2023-03-15 14:47:30 +0100

    #24

--- src/Controller/SupprimerSonCompteController.php
+++ src/Controller/SupprimerSonCompteController.php
@@ -16,27 +16,27 @@ class SupprimerSonCompteController extends AbstractController
     public function index(UtilisateurRepository $utilisateurRepository = null): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY'); //L'utilisateur est authentifié
-        $mail =  $this->getUser()->getUserIdentifier();
-        $utilisateur = $utilisateurRepository->rechercher($mail);
         return $this->render('supprimer_son_compte/index.html.twig', [
-            'controller_name' => 'SupprimerSonCompteController',
-            'utilisateur'   => $utilisateur
+            'controller_name' => 'SupprimerSonCompteController'
         ]);
     }
 
     #[Route('/supprimerSonComptePerso', name: 'utilisateur.supprimerComptePerso')]
     public function supprimerSonComptePerso(UtilisateurRepository $utilisateurRepository = null, ManagerRegistry $doctrine ) : RedirectResponse {
+        
             $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY'); //L'utilisateur est authentifié
+        
             $mail =  $this->getUser()->getUserIdentifier();
             $utilisateur = $utilisateurRepository->rechercher($mail);
             if($utilisateur){
                 if(($utilisateur->getTrajets()->isEmpty())){
                     if(($utilisateur->getTrajetProposes()->isEmpty())){
+                        $this->container->get('security.token_storage')->setToken(null);
                         $manager = $doctrine->getManager();
                         $manager->remove($utilisateur);
                         $manager->flush();
                         $this->addFlash('success','Votre compte a été supprimer avec succès');
-                        return $this->redirectToRoute("app_login");
+                        return $this->redirectToRoute("app_logout");
                     }else{
                         $this->addFlash('error','Suppression impossible : vous avez déposer une demande de covoiturage encore en cours');
                     }
