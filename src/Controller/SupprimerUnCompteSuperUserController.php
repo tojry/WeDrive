c721b9ed1eb9c7af11ecd616a9c73119781f899f
nyela20 <ralantonisainananyela@gmail.com>
2023-03-16 10:57:13 +0100

    #18

--- src/Controller/SupprimerUnCompteSuperUserController.php
+++ src/Controller/SupprimerUnCompteSuperUserController.php
@@ -7,13 +7,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\UtilisateurRepository;
 use App\Entity\Utilisateur;
 
 class SupprimerUnCompteSuperUserController extends AbstractController
 {
     #[Route('/supprimeruncomptesuperuser', name: 'app_supprimer_un_compte_super_user')]
-    public function index(ManagerRegistry $doctrine): Response
+    public function index(ManagerRegistry $doctrine, UtilisateurRepository $utilisateurRepository = null): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY'); 
+        $mail =  $this->getUser()->getUserIdentifier();
+        $utilisateur = $utilisateurRepository->rechercher($mail);
+        if($utilisateur->getRoles()[0] == "ROLE_USER"){
+            return $this->render('supprimer_un_compte_super_user/accesrefuse.html.twig', [
+                'controller_name' => 'AfficherListeCompteSuperUserController',
+            ]);
+        }
         $repository = $doctrine->getRepository(Utilisateur::class);
         $utilisateurs = $repository->findAll();
         return $this->render('supprimer_un_compte_super_user/index.html.twig', [
@@ -24,6 +33,7 @@ class SupprimerUnCompteSuperUserController extends AbstractController
 
     #[Route('/supprimerCompte/{id}', name: 'utilisateur.supprimerCompte')]
     public function supprimerUnCompteSuperUser(Utilisateur $utilisateur = null, ManagerRegistry $doctrine ) : RedirectResponse {
+
         // Récuperer la personne
         if($utilisateur){
             // Si la personne existe => le supprimer et retourner un flashMessage de succès
