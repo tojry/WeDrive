d24f271b352946dc7bc52feb7e50d3b0f43c0b3b
nyela20 <ralantonisainananyela@gmail.com>
2023-03-16 09:59:20 +0100

    #24

--- src/Controller/SupprimerSonCompteController.php
+++ src/Controller/SupprimerSonCompteController.php
@@ -6,9 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\UtilisateurRepository;
 use App\Entity\Utilisateur;
+
 
 class SupprimerSonCompteController extends AbstractController
 {
@@ -21,8 +23,8 @@ class SupprimerSonCompteController extends AbstractController
         ]);
     }
 
-    #[Route('/supprimerSonComptePerso', name: 'utilisateur.supprimerComptePerso')]
-    public function supprimerSonComptePerso(UtilisateurRepository $utilisateurRepository = null, ManagerRegistry $doctrine ) : RedirectResponse {
+    #[Route('/supprimerSonComptePerso/{pwd}', name: 'utilisateur.supprimerComptePerso')]
+    public function supprimerSonComptePerso(?string $pwd = null,UtilisateurRepository $utilisateurRepository = null, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher) : RedirectResponse {
         
             $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY'); //L'utilisateur est authentifié
         
@@ -31,14 +33,25 @@ class SupprimerSonCompteController extends AbstractController
             if($utilisateur){
                 if(($utilisateur->getTrajets()->isEmpty())){
                     if(($utilisateur->getTrajetProposes()->isEmpty())){
-                        $this->container->get('security.token_storage')->setToken(null);
-                        $manager = $doctrine->getManager();
-                        $manager->remove($utilisateur);
-                        $manager->flush();
-                        $this->addFlash('success','Votre compte a été supprimer avec succès');
-                        return $this->redirectToRoute("app_logout");
+                        //verifier mot de passe 
+                        if($pwd == null){
+                            $this->addFlash('error',"Veuillez entrer votre mot de passe afin de confirmer la suppression de votre compte");
+                        }
+                        else{
+                            $hashedPassword = $passwordHasher->hashPassword(  $utilisateur, $pwd );
+                            if(strcmp($utilisateur->getPassword(), $hashedPassword) != 0){
+                                $this->container->get('security.token_storage')->setToken(null);
+                                $manager = $doctrine->getManager();
+                                $manager->remove($utilisateur);
+                                $manager->flush();
+                                return $this->redirectToRoute("app_logout");
+                            }
+                            else{
+                                $this->addFlash('error',"mot de passe incorrecte");
+                            }           
+                        }
                     }else{
-                        $this->addFlash('error','Suppression impossible : vous avez déposer une demande de covoiturage encore en cours');
+                        $this->addFlash('error','Suppression impossible : vous avez déposer une demande de covoiturage encore en cours de validité');
                     }
                 }
                 else{
