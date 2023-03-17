60306e525f00426293760bc046db7ff318773493
rafed-S <rafed.safour2@etu.univ-lorraine.fr>
2023-03-17 15:56:57 +0100

    push Consulter Groupes Controller

--- src/Controller/ConsulterGroupesController.php
+++ src/Controller/ConsulterGroupesController.php
@@ -5,14 +5,43 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\GroupeAmisRepository;
 
 class ConsulterGroupesController extends AbstractController
 {
+    /*
     #[Route('/consulter/groupes', name: 'app_consulter_groupes')]
+    
     public function index(): Response
     {
         return $this->render('consulter_groupes/index.html.twig', [
             'controller_name' => 'ConsulterGroupesController',
+        ]);
+    }*/
+
+    #[Route('/groupes', name: 'app_consulter_groupes')]
+    public function index(GroupeAmisRepository $groupeAmisRepository): Response
+    {
+        $groupes = $groupeAmisRepository->findAll();
+
+        return $this->render('consulter_groupes/index.html.twig', [
+            'controller_name' => 'ConsulterGroupesController',
+            'groupes' => $groupes,
+        ]);
+    }
+
+    #[Route('/groupes/{id}', name: 'app_consulter_groupe_details', methods: ['GET'])]
+    public function show(int $id, GroupeAmisRepository $groupeAmisRepository): Response
+    {
+        $groupe = $groupeAmisRepository->find($id);
+
+        if (!$groupe) {
+            throw $this->createNotFoundException('Le groupe demandé n\'existe pas.');
+        }
+
+        return $this->render('consulter_groupes/details.html.twig', [
+            'controller_name' => 'ConsulterGroupesController',
+            'groupe' => $groupe,
         ]);
     }
 }
