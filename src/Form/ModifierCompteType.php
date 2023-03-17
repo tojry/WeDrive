afadff10286d51a60c51d3b7c6934d6062c5e6bd
aboubacar <hassanec3u@etu.univ-lorraine.fr>
2023-03-17 19:10:13 +0100

    lien consulter compte - modifier compte fini

--- src/Form/ModifierCompteType.php
+++ src/Form/ModifierCompteType.php
@@ -30,7 +30,7 @@ class ModifierCompteType extends AbstractType
             ->add('voiture', CheckboxType::class, ['label' => 'J\'ai un véhicule à ma disposition :', 'required' => false])
             ->add('mailNotif', CheckboxType::class, ['label' => 'J\'accepte de recevoir des notifications par mail de la part de WeDrive :', 'required' => false])
             ->add('creer', SubmitType::class, ['label' => 'Valider'])
-            ->add('supprimer', SubmitType::class, ['label' => 'Supprimer mon compte', 'attr' => [ 'class' => 'is-danger', 'onclick' => 'suppresionCompte()' ]]);
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
