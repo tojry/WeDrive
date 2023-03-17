14fc17d1710b063dba40e450c5cb061c1b094099
rafed-S <rafed.safour2@etu.univ-lorraine.fr>
2023-03-17 23:22:31 +0100

    Modifier (ConsulterGroupesController)

--- src/Controller/ConsulterGroupesController.php
+++ src/Controller/ConsulterGroupesController.php
@@ -9,39 +9,14 @@ use App\Repository\GroupeAmisRepository;
 
 class ConsulterGroupesController extends AbstractController
 {
-    /*
-    #[Route('/consulter/groupes', name: 'app_consulter_groupes')]
-    
-    public function index(): Response
-    {
-        return $this->render('consulter_groupes/index.html.twig', [
-            'controller_name' => 'ConsulterGroupesController',
-        ]);
-    }*/
-
     #[Route('/groupes', name: 'app_consulter_groupes')]
     public function index(GroupeAmisRepository $groupeAmisRepository): Response
     {
         $groupes = $groupeAmisRepository->findAll();
 
         return $this->render('consulter_groupes/index.html.twig', [
-            'controller_name' => 'ConsulterGroupesController',
             'groupes' => $groupes,
         ]);
     }
 
-    #[Route('/groupes/{id}', name: 'app_consulter_groupe_details', methods: ['GET'])]
-    public function show(int $id, GroupeAmisRepository $groupeAmisRepository): Response
-    {
-        $groupe = $groupeAmisRepository->find($id);
-
-        if (!$groupe) {
-            throw $this->createNotFoundException('Le groupe demandé n\'existe pas.');
-        }
-
-        return $this->render('consulter_groupes/details.html.twig', [
-            'controller_name' => 'ConsulterGroupesController',
-            'groupe' => $groupe,
-        ]);
-    }
 }
