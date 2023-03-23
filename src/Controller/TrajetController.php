2f17767bd511c92496f4221170fd4c4cedc59607
tojry <123320828+tojry@users.noreply.github.com>
2023-03-23 10:59:28 +0100

    Update TrajetController.php

--- src/Controller/TrajetController.php
+++ src/Controller/TrajetController.php
@@ -18,7 +18,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrajetController extends AbstractController
 {
