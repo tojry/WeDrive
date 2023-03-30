098000028ac6b1a709f1424b2f471c0662de6bc1
slimani44u <slimani44u@etu.univ-lorraine.fr>
2023-03-30 12:20:54 +0200

    resolution pb de modif ville

--- src/Controller/ModifierOffreController.php
+++ src/Controller/ModifierOffreController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\PointIntermediaire;
 use App\Entity\Trajet;
+use App\Entity\Ville;
 use App\Form\CreerFormType;
 use App\Repository\TrajetRepository;
 use App\Repository\UtilisateurRepository;
@@ -17,22 +18,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class ModifierOffreController extends AbstractController
 {
     #[Route('/modifierOffre/{id}', name: 'app_modifier_offre')]
-    public function modifier(Request $request, Trajet $trajet): Response
+    public function modifier(Request $request, Trajet $trajet, EntityManagerInterface $entityManager, VilleRepository $villes): Response
     {
+
         $form = $this->createForm(CreerFormType::class, $trajet);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            // Vérifier si les informations sont différentes
+            $formValues = $form->getData();
+            $valdep = $form['lieuDepart']->getData();
+            $valArr= $form['lieuArrive']->getData();
+            $villeArr = $villes->rechercher($valArr);
 
+            $villeDep = $villes->rechercher($valdep);
+            $villeArr = $villes->rechercher($valArr);
+            //echo $valdep;
+            if($villeDep != null){
+                echo $villeDep[0];
+
+                $trajet->setLieuDepart($villeDep[0]);
+            }
+            if($villeArr != null){
+                echo $villeArr[0];
+
+                $trajet->setLieuArrive($villeArr[0]);
+            }
+
+
+            echo"ici";
+            // Enregistrer les modifications
+            $entityManager->persist($trajet);
+            $entityManager->flush();
             $this->addFlash('success', 'L\'offre a bien été modifiée.');
+        } else {
+            $this->addFlash('warning', 'Aucune modification n\'a été enregistrée.');
+            }
 
-        }
 
         return $this->render('modifier_offre/index.html.twig', [
             'creer_form' => $form->createView(),
             'trajet' => $trajet,
         ]);
     }
-
 }
