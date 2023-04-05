20b8333bbae2f28316ab567020da2da3e8fdb7b6
tojry <theojoffroy@gmail.com>
2023-04-05 18:54:39 +0200

    Tentative de correction modifier mon compte

--- src/Controller/ModifierSonCompteController.php
+++ src/Controller/ModifierSonCompteController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
 use App\Form\ModifierCompteType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class ModifierSonCompteController extends AbstractController
@@ -23,21 +24,24 @@ class ModifierSonCompteController extends AbstractController
         $this->security = $security;
     }
 
-    #[Route('/modifierSonCompte', name: 'app_modifier_son_compte')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/modifierSonCompte/{id}', name: 'app_modifier_son_compte')]
+    public function index(Request $request, Utilisateur $user, EntityManagerInterface $entityManager): Response
     {
 
-        $user = $this->security->getUser();
+        $user_co = $this->getUser();
+
+        if($user_co != $user){
+            return new Response("Vous n'avez pas accès à cette page");
+        }
+
         $form = $this->createForm(ModifierCompteType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $task = $form->getData();
-
 
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirectToRoute('app_consultercompte', array('id' => $user->getId()));
+            return $this->redirectToRoute('app_consultercompte', ['id' => $user->getId()]);
         }
 
         return $this->render('modifier_son_compte/modifierCompte.html.twig', [
